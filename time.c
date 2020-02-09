c563e4d049722e7392293ab8d4eefd49b1834085
JMJ Riyana <jeemariyana@gmail.com>
2020-02-09 20:32:03 +0530

    time

--- time.c
+++ time.c
@@ -2,30 +2,30 @@
 #include<math.h>
 #include<string.h>
 int main() {
-    int hh, mm, ss ;
+    int HH, MM, SS ;
     char a;
-    scanf("%d:%d:%d%c",&hh,&mm,&ss,&a);
+    scanf("%d:%d:%d%c",&HH,&MM,&SS,&a);
     
     if(a=='A')
     {
-      if(hh==12)
+      if(HH==12)
       {
-          printf("%02d:%02d:%02d",00,mm,ss);
+          printf("%02d:%02d:%02d",00,MM,SS);
       }
       else 
        {
-       printf("%02d:%02d:%02d",hh,mm,ss);
+       printf("%02d:%02d:%02d",HH,MM,SS);
       }
     }
     else if(a=='P')
     { 
-        if(hh<12)
+        if(HH<12)
         {
-        printf("%02d:%02d:%02d",hh+12,mm,ss);
+        printf("%02d:%02d:%02d",HH+12,MM,SS);
         }
         else
         {
-        printf("%02d:%02d:%02d",hh,mm,ss);
+        printf("%02d:%02d:%02d",HH,MM,SS);
         }       
          }
     return 0;
